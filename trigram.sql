af6782c654c27cb5f7a083630b5a9d9941f2b053
Duc <ductran032118@gmail.com>
2023-10-24 21:30:32 +0700

    fix minor errors in sql functions

--- trigram.sql
+++ trigram.sql
@@ -1,42 +1,4 @@
-CREATE OR REPLACE FUNCTION public.generatetrigram(string text)
- RETURNS TABLE(trigram text)
- LANGUAGE plpgsql
-AS $function$
-	declare top int;
-	begin
-		top = case
-			when length(string) > 2 then length(string) - 2
-			else 0
-		end;
-		return query with N16 as 
-		(
-			select V.v from 
-			(
-				values
-					(0),(0),(0),(0),(0),(0),(0),(0),
-					(0),(0),(0),(0),(0),(0),(0),(0)
-			) as V (v)
-		),
-		Nums as
-		(
-			select cast(row_number() over (order by A.v) as int) n
-			from N16 as A
-			cross join N16 as B
-		),
-		Trigrams as 
-		(
-			select substring(string, N.n, 3) trigram from Nums as N order by N.n limit top
-		) 	
-		
-		select distinct T.trigram
-			from Trigrams as T 
---			where T.trigram collate Latin1_General_BIN2 
-			where T.trigram not like '%[^A-Z0-9a-z]%';
-	end;
-$function$
-;
-
-create extension pg_trgm;
+create extension if not exists pg_trgm;
 
 create table if not exists Trigrams as (
   select
@@ -45,7 +7,7 @@ create table if not exists Trigrams as (
     bstrings20,
     lateral unnest(show_trgm(bstrings20.phash)) as trgm
   where 
-    length(trim(trgm)) = 3;
+    length(trim(trgm)) = 3
 );
 
 -- create and index view over trigrams table to make it fast and easy to find 
@@ -109,7 +71,7 @@ as $function$
 			end if;
 			-- 1 available trigram
 			return query
-				select id from trigrams t1 where t1.trigram = trm1;
+				select id from trigrams t1 where t1.trigram = trgm1;
 		end if;
 	end;
 $function$;
@@ -123,10 +85,10 @@ as $function$
 	begin
 		
 		return query 
-			select r.string from getbesttrigrams(searchterm) gbt,
+			select r.phash from getbesttrigrams(searchterm) gbt,
 			lateral (
 				-- trigram search
-				select e.id, e.phash from getmatchingtrigramid() as mid
+				select e.id, e.phash from getmatchingtrigramid(gbt.trgm1, gbt.trgm2, gbt.trgm3) as mid
 				join bstrings20 as e on e.id = mid.id
 				where gbt.trgm1 is not null and e.phash like searchterm
 				
